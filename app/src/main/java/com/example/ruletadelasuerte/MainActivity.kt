96960c3aff9c7a1afe41e046949c462708844ef5
Daniel <daniel.bodero.velasco@iesjulianmarias.es>
2025-01-03 13:48:03 +0100

    Creación de lo básico

--- app/src/main/java/com/example/ruletadelasuerte/MainActivity.kt
+++ app/src/main/java/com/example/ruletadelasuerte/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ruletadelasuerte
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.empezar)?.setOnClickListener{
+            val intent = Intent(this, PlayerSelect::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.button2)?.setOnClickListener{
+
+        }
+
+        findViewById<Button>(R.id.button3)?.setOnClickListener{
+
+        }
+
+        findViewById<Button>(R.id.configuracion)?.setOnClickListener{
+
         }
     }
 }
