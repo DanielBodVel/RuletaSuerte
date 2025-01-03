3c79307becc8a3ac0357569392a22e0f1c683bfc
Daniel <daniel.bodero.velasco@iesjulianmarias.es>
2025-01-03 18:48:13 +0100

    Añado un botón

--- app/src/main/java/com/example/ruletadelasuerte/PlayerSelect.kt
+++ app/src/main/java/com/example/ruletadelasuerte/PlayerSelect.kt
@@ -1,6 +1,8 @@
 package com.example.ruletadelasuerte
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class PlayerSelect : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.next)?.setOnClickListener{
+            val intent = Intent(this, TalkActivity::class.java)
+            startActivity(intent)
         }
     }
 }
